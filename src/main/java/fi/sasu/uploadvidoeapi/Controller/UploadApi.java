a6828071af517a823752adb6cb7136cfb92f4be5
Sasu <sasuolander@yahoo.com>
2021-12-31 18:52:49 +0200

    simple uploading is now working

--- src/main/java/fi/sasu/uploadvidoeapi/Controller/UploadApi.java
+++ src/main/java/fi/sasu/uploadvidoeapi/Controller/UploadApi.java
@@ -10,13 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
-import static fi.sasu.uploadvidoeapi.Controller.constant.FILEPATH;
+import static fi.sasu.uploadvidoeapi.Controller.Util.decodeBase64;
+import static fi.sasu.uploadvidoeapi.Controller.Constant.FILEPATH;
 
-class constant {
+class Constant {
+    private Constant() {
+    }
+
     static final String FILEPATH = "./temp/";
+
 }
 
 enum FileExtension {
@@ -44,69 +48,71 @@ public class UploadApi {
 
     @PostMapping("/download")
     public String downloadApi(@RequestBody Inputs inputs) {
-        logger.info(inputs.input());
-        logger.info(inputs.input2());
         logger.info("main thread " + Thread.currentThread().getName());
-        ThreadForDownload test1 = new ThreadForDownload(inputs.input());
-        ThreadForDownload test2 = new ThreadForDownload(inputs.input2());
-        test2.start();
-        test1.start();
+
+        // factory to generate and start thread
+        RunnableForDownload test1 = new RunnableForDownload(inputs.input(), "", FileExtension.MP4);
+        RunnableForDownload test2 = new RunnableForDownload(inputs.input2(), "", FileExtension.MP4);
+
+        Thread thread1 = new Thread(test1);
+        Thread thread2 = new Thread(test2);
+        thread1.start();
+        thread2.start();
+
+        // wait until all thread are finished then tell success,
+        // Monitor Status of thread and send information for a failure
+
         return "success";
     }
 }
 
-// how to model video as byte
-// how playable it is
 record Inputs(String input, String input2) {
 }
 
 record Inputs2(ArrayList<String> videos) {
 }
 
-class ThreadForDownload extends Thread {
-    Logger logger = (Logger) LoggerFactory.getLogger(ThreadForDownload.class);
-    volatile Long status;
-    String singletonFilename = Integer.toHexString(new Random().nextInt());
-    String input;
+class RunnableForDownload implements Runnable {
+    Logger logger = (Logger) LoggerFactory.getLogger(RunnableForDownload.class);
+    final String singletonIdentifier = Integer.toHexString(new Random().nextInt());
+    private final String input;
+    private final String path;
 
-    ThreadForDownload(String input) {
+    RunnableForDownload(@NotNull String input, @NotNull String fileName, @NotNull FileExtension extension) {
         this.input = input;
+        this.path = FILEPATH + fileName + singletonIdentifier + extension.getExtension();
     }
 
     @Override
     public void run() {
-        logger.info("thread name " + Thread.currentThread().getName());
-        logger.info("ThreadForDownload  " + this.input);
-        createFile("test", FileExtension.TXT);
-        writeMethod(this.input, "test", FileExtension.TXT);
+        logger.info("thread name {}", Thread.currentThread().getName());
+        logger.info("ThreadForDownload {}", this.input);
+        createFile();
+        writeMethod();
     }
 
-    public String createFile(String filename, FileExtension extension) {
+    public void createFile() {
         try {
-            File file = new File(FILEPATH + filename + this.singletonFilename + extension.getExtension());
+            File file = new File(this.path);
             if (file.createNewFile()) {
-
                 logger.info("File created: " + file.getName());
-                return file.getPath();
             } else {
                 logger.info("File already exists.");
-                return file.getPath();
             }
         } catch (IOException e) {
             logger.info("An error occurred.");
             e.printStackTrace();
-            return null;
         }
     }
 
 
-    public void writeMethod(String input, String filename, FileExtension extension) {
+    public void writeMethod() {
         try {
-
-            FileWriter myWriter = new FileWriter(FILEPATH + filename + this.singletonFilename + extension.getExtension());
-            myWriter.write(input);
-            myWriter.close();
-            logger.info("Successfully wrote to the file.");
+            byte[] decoded = decodeBase64(this.input);
+            try (FileOutputStream myWriter = new FileOutputStream(this.path)) {
+                myWriter.write(decoded);
+                logger.info("Successfully wrote to the file.");
+            }
         } catch (IOException e) {
             logger.info("An error occurred.");
             e.printStackTrace();
@@ -116,14 +122,11 @@ class ThreadForDownload extends Thread {
 }
 
 class Util {
-    public static @NotNull String decodeBase64(String input) {
-        Base64 base64 = new Base64();
-        return new String(base64.decode(input.getBytes()));
+    private Util() {
     }
 
-    public static @NotNull String encodeBase64(String input) {
-        String originalInput = "test input";
+    public static byte[] decodeBase64(String input) {
         Base64 base64 = new Base64();
-        return new String(base64.encode(input.getBytes()));
+        return base64.decode(input.getBytes());
     }
 }
